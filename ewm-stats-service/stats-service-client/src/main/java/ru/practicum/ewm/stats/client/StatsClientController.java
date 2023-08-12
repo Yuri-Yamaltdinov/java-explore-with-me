4fd17b43b89a2f0cd62e9530d788afda566812b9
Yuri Yamaltdinov <yyamaltdinov@yandex.ru>
2023-08-12 21:04:57 +0300

    refactor: checkstyle fix

--- ewm-stats-service/stats-service-client/src/main/java/ru/practicum/ewm/stats/client/StatsClientController.java
+++ ewm-stats-service/stats-service-client/src/main/java/ru/practicum/ewm/stats/client/StatsClientController.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.stats.client;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.stats.dto.HitRequestDto;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping
@@ -31,28 +28,15 @@ public class StatsClientController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-/*    @GetMapping("/stats")
-    public ResponseEntity<Object> getStats(
-                            @RequestParam(name = "start", required = true)
-                            @DateTimeFormat(pattern = DATE_TIME_PATTERN) LocalDateTime start,
-                            @RequestParam(name = "end", required = true)
-                            @DateTimeFormat(pattern = DATE_TIME_PATTERN) LocalDateTime end,
-                            @RequestParam(name = "uris", required = false) List<String> uris,
-                            @RequestParam(name = "unique", required = false, defaultValue = "false") Boolean unique) {
-        log.info("Got request GET /stats with start: {}, end: {}, uris: {}, unique: {}",
-                start, end, uris, unique);
-
-        return statsClient.getStatistics(start, end, uris, unique);
-    }*/
     @GetMapping("/stats")
     public ResponseEntity<Object> getStats(@RequestParam String start,
                                            @RequestParam String end,
                                            @RequestParam(required = false) String[] uris,
                                            @RequestParam(required = false, defaultValue = "false") Boolean unique) {
-    log.info("Got request GET /stats with start: {}, end: {}, uris: {}, unique: {}",
-            start, end, uris, unique);
+        log.info("Got request GET /stats with start: {}, end: {}, uris: {}, unique: {}",
+                start, end, uris, unique);
 
-    return statsClient.getStatistics(start, end, uris, unique);
-}
+        return statsClient.getStatistics(start, end, uris, unique);
+    }
 
 }
