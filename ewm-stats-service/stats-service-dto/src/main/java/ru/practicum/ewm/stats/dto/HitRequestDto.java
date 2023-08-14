e93bd8ee19cb41e9a7e0527e558a4d7ec1d2c230
Yuri Yamaltdinov <yyamaltdinov@yandex.ru>
2023-08-14 18:03:33 +0300

    feat: docker-compose configuration

--- ewm-stats-service/stats-service-dto/src/main/java/ru/practicum/ewm/stats/dto/HitRequestDto.java
+++ ewm-stats-service/stats-service-dto/src/main/java/ru/practicum/ewm/stats/dto/HitRequestDto.java
@@ -1,14 +1,18 @@
 package ru.practicum.ewm.stats.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HitRequestDto {
     @NotBlank(message = "Field app must be filled.")
     String app;
