013a4a97f9ec3927d7b95ba890696990a8af8c4a
Yuri Yamaltdinov <yyamaltdinov@yandex.ru>
2023-08-24 22:47:22 +0300

    fix: add start and end time check

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/ewm/stats/service/controller/StatsController.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/ewm/stats/service/controller/StatsController.java
@@ -3,18 +3,20 @@ package ru.practicum.ewm.stats.service.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.stats.dto.HitRequestDto;
 import ru.practicum.ewm.stats.dto.ViewStatsResponseDto;
 import ru.practicum.ewm.stats.service.service.StatsService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.net.URLDecoder;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static ru.practicum.ewm.stats.dto.util.Constants.DATE_FORMAT;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static ru.practicum.ewm.stats.dto.util.Constants.FORMATTER;
 
 @RestController
 @RequestMapping
@@ -33,17 +35,16 @@ public class StatsController {
 
     @GetMapping("/stats")
     @ResponseStatus(HttpStatus.OK)
-    public List<ViewStatsResponseDto> getStats(
-            @RequestParam(name = "start", required = true)
-            @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime start,
-            @RequestParam(name = "end", required = true)
-            @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime end,
-            @RequestParam(name = "uris", required = false) List<String> uris,
-            @RequestParam(name = "unique", required = false, defaultValue = "false") boolean unique
-    ) {
+    public List<ViewStatsResponseDto> getStats(@RequestParam String start,
+                                               @RequestParam String end,
+                                               @RequestParam(required = false) List<String> uris,
+                                               @RequestParam(required = false, defaultValue = "false") Boolean unique,
+                                               HttpServletRequest request) {
         log.info("Got request GET /stats, with start: {}, end: {}, uris: {}, unique: {}",
                 start, end, uris, unique);
-        return statsService.getStats(start, end, uris, unique);
+        LocalDateTime startTime = LocalDateTime.parse(URLDecoder.decode(start, UTF_8), FORMATTER);
+        LocalDateTime endTime = LocalDateTime.parse(URLDecoder.decode(end, UTF_8), FORMATTER);
+        return statsService.getStats(startTime, endTime, uris, unique);
     }
 
 }
--- ewm-stats-service/stats-service/src/main/java/ru/practicum/ewm/stats/service/service/StatsServiceImpl.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/ewm/stats/service/service/StatsServiceImpl.java
@@ -8,8 +8,10 @@ import ru.practicum.ewm.stats.dto.ViewStatsResponseDto;
 import ru.practicum.ewm.stats.service.mapper.HitMapper;
 import ru.practicum.ewm.stats.service.mapper.ViewStatsMapper;
 import ru.practicum.ewm.stats.service.model.Hit;
+import ru.practicum.ewm.stats.service.model.ViewStats;
 import ru.practicum.ewm.stats.service.repository.StatsRepository;
 
+import javax.validation.ValidationException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,22 +33,19 @@ public class StatsServiceImpl implements StatsService {
     @Override
     public List<ViewStatsResponseDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
 
-        if (unique && uris != null) {
-            return statsRepository.getUrisWithUniqueIP(start, end, uris).stream()
-                    .map(viewMapper::dtoFromView)
-                    .collect(Collectors.toList());
-        } else if (uris != null) {
-            return statsRepository.getUrisStats(start, end, uris).stream()
-                    .map(viewMapper::dtoFromView)
-                    .collect(Collectors.toList());
-        } else if (unique) {
-            return statsRepository.getAllUrisWithUniqueIP(start, end).stream()
-                    .map(viewMapper::dtoFromView)
-                    .collect(Collectors.toList());
-        } else {
-            return statsRepository.getAllUrisStats(start, end).stream()
-                    .map(viewMapper::dtoFromView)
-                    .collect(Collectors.toList());
+        if (start != null && start.isAfter(end)) {
+            throw new ValidationException("Start time can not be later than End time.");
         }
+        List<ViewStats> result;
+        if (uris != null) {
+            result = (unique) ? statsRepository.getUrisWithUniqueIP(start, end, uris) :
+                    statsRepository.getUrisStats(start, end, uris);
+        } else {
+            result = (unique) ? statsRepository.getAllUrisWithUniqueIP(start, end) :
+                    statsRepository.getAllUrisStats(start, end);
+        }
+        return result.stream()
+                .map(viewMapper::dtoFromView)
+                .collect(Collectors.toList());
     }
 }
