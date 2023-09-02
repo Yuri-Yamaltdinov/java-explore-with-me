8cfdb573bd322105f704c02991b87767847b3a31
Yuri Yamaltdinov <yyamaltdinov@yandex.ru>
2023-09-02 15:17:48 +0300

    refactor: replace range time from string to localdatetime

--- ewm-main-service/main-server/src/main/java/ru/practicum/ewm/main/service/event/EventControllerPublic.java
+++ ewm-main-service/main-server/src/main/java/ru/practicum/ewm/main/service/event/EventControllerPublic.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm.main.service.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ import javax.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static ru.practicum.ewm.main.service.util.Constants.FORMATTER;
+import static ru.practicum.ewm.main.service.util.Constants.DATE_FORMAT;
 
 @RestController
 @RequestMapping("/events")
@@ -26,6 +27,7 @@ import static ru.practicum.ewm.main.service.util.Constants.FORMATTER;
 @Slf4j
 public class EventControllerPublic {
     private final EventService eventService;
+/*
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -44,6 +46,28 @@ public class EventControllerPublic {
         LocalDateTime start = (rangeStart == null) ? null : LocalDateTime.parse(rangeStart, FORMATTER);
         LocalDateTime end = (rangeEnd == null) ? null : LocalDateTime.parse(rangeEnd, FORMATTER);
         return eventService.getAllEventsPublic(text, categories, paid, start, end, onlyAvailable, sort, from, size, request);
+    }
+*/
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<EventShortDto> getAllEvents(@RequestParam(required = false) String text,
+                                            @RequestParam(required = false) List<Long> categories,
+                                            @RequestParam(required = false) Boolean paid,
+                                            @RequestParam(required = false)
+                                            @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime rangeStart,
+                                            @RequestParam(required = false)
+                                            @DateTimeFormat(pattern = DATE_FORMAT) LocalDateTime rangeEnd,
+                                            @RequestParam(defaultValue = "false") Boolean onlyAvailable,
+                                            @RequestParam(required = false) String sort,
+                                            @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
+                                            @Positive @RequestParam(defaultValue = "10") Integer size,
+                                            HttpServletRequest request) {
+        log.info("Got request GET all events with parameters: text = {}, categories = {}, paid={}, rangeStart = {}, rangeEnd = {}" +
+                ", onlyAvailable={}, sort={}, from = {}, size = {}", text, categories, paid, rangeStart, rangeEnd, onlyAvailable, sort, from, size);
+        //LocalDateTime start = (rangeStart == null) ? null : LocalDateTime.parse(rangeStart, FORMATTER);
+        //LocalDateTime end = (rangeEnd == null) ? null : LocalDateTime.parse(rangeEnd, FORMATTER);
+        return eventService.getAllEventsPublic(text, categories, paid, rangeStart, rangeEnd, onlyAvailable, sort, from, size, request);
     }
 
     @GetMapping("/{eventId}")
